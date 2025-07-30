13817b2ecbc471945faecd19bc4a5873a32366e8
Simon Tech <contato.simon.tech@gmail.com>
2025-07-30 09:38:00 -0300

    aula 09

---
+++ editprofile.php
@@ -0,0 +1,12 @@
+<?php
+require_once("templates/header.php");
+
+?>
+
+<div class="container-fluid" id="main-container">
+    <h1>Editar Perfil</h1>
+</div>
+
+<?php
+require_once("templates/footer.php");
+?>
